e704dcbb170c74e7277173c2761c8c5c74dfeee8
Adham Mahmoud <adhammahmoudrm07@gmail.com>
2026-08-06 18:16:37 +0300

    task2

--- python-for-ml/Session1/assignment/task2.py
+++ python-for-ml/Session1/assignment/task2.py
@@ -1,16 +1,18 @@
 # 1. النص المشفر
 x = "###!!@mocleW EPGTQ!!!6789"
 
-# 2. استخراج النص الأساسي (بنضيف +1 لـ index الـ q عشان الحرف يتدخل معانا)
-core = x[x.index('m') : x.index('Q') + 1]  # "mocleW EPGTQ"
+core = x[x.index('m') : x.index('Q') + 1]  
 
-# 3. فصل الكلمتين
+
 word1, word2 = core.split()
 
-# 4. عكس الكلمة الأولى
-word1_reversed = word1[::-1]  # "Welcome"
+word1_reversed = word1[::-1]  
 
-# 5. تجميع الرسالة النهائية
-decoded_message = f"{word1_reversed} {word2}"
+word2_edited = ""
+for i in range(len(word2)):
+    if word2[i] in ['a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U']:
+        continue
+    else:
+        word2_edited += word2[i]
 
-print(decoded_message)  # النتيجة: Welcome EPGTQ
+print(word1_reversed, word2_edited)
